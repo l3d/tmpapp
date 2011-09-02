b2d0fd247b93e2db3713de042b63ff65fd6b227a
l3d <l3d@l3ds-macbook-pro-2.local>
2011-09-02 16:25:57 -0700

    Inital Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = 'e1aa134b6916f895c69c4052283c613ca0a3daa987adadea3b8a84f559274b020a423c9be67f175fd7bdfeed138505b65c2a6fa019b1db1307f3a99345e264e7'
